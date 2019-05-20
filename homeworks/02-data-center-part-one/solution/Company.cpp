3903ab442e869d6086ae7bcc9fc39c31d91b0c47
Deian Goranov <deian.goranov@gmail.com>
2019-05-21 00:59:55 +0300

    Fix wrong copy

--- homeworks/02-data-center-part-one/solution/Company.cpp
+++ homeworks/02-data-center-part-one/solution/Company.cpp
@@ -1,45 +1,30 @@
 // Разделянето на дефинициите на член-функциите на Company в отделен файл позволява
 // цикличната зависимост между Company и StorageUnit да не прави проблем.
+#include <cstring>
+
 #include "Company.h"
 
-void DataCenter::lendStorageTo (const Company& company) {
-	StorageUnit* freeUnit = nullptr;
 
-	for (unsigned int i = 0, storageSize = storage.getSize(); i < storageSize; ++i) {
-		if (storage[i].getOwner() == nullptr) {
-			freeUnit = &storage[i];
-			break;
-		}
-	}
-
-	if (freeUnit != nullptr) {
-		Company* targetClient = nullptr;
-		unsigned int clientsSize = clients.getSize();
-
-		for (unsigned int i = 0; i < clientsSize; ++i) {
-			if (clients[i] == company) {
-				targetClient = &clients[i];
-			}
-		}
-
-		if (targetClient == nullptr) {
-			clients.pushBack(company);
-			targetClient = &clients[clientsSize];
-		}
-
-		targetClient->addStorage(*freeUnit);
-		freeUnit->setOwner(targetClient);
-	}
+Company::Company () {
+	budget = 0;
 }
 
-void DataCenter::taxClients () {
-	for (unsigned int i = 0, clientsSize = clients.getSize(); i < clientsSize; ++i) {
-		budget += clients[i].payForUnits();
-	}
+void Company::addStorage (const StorageUnit& storageUnit) {
+	rentedStorage.pushBack(storageUnit);
 }
 
-void DataCenter::upgradeStorage (const DynamicArray <StorageUnit>& disks) {
-	for (unsigned int i = 0, disksSize = disks.getSize(); i < disksSize; ++i) {
-		storage.pushBack(disks[i]);
+double Company::payForUnits () {
+	double money = 0;
+
+	for (unsigned int i = 0, storageSize = rentedStorage.getSize(); i < storageSize; ++i) {
+		double cost = TAX_RATE * rentedStorage[i].getCost();
+		budget -= cost;
+		money += cost;
 	}
+
+	return money;
+}
+
+bool Company::operator== (const Company& other) const {
+	return strcmp(name, other.name) == 0;
 }
